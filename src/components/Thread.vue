59ebfcc7ecfa560e4140edaea1c2f7a74c9a0f8f
zhaozhentao <344696734@qq.com>
2020-02-02 13:00:30 +0800

    线程堆栈

--- src/components/Thread.vue
+++ src/components/Thread.vue
@@ -5,20 +5,33 @@
       <ve-line :data="form" :colors="colors" v-if="!loading"/>
     </div>
 
+    <button class="btn btn-default" v-on:click="getThreads">
+      <span class="glyphicon glyphicon-refresh" aria-hidden="true"></span>刷新
+    </button>
+
     <div class="row">
-      <div class="col-md-12">
+      <div class="col-md-6">
         <table class="table table-hover table-bordered">
+          <thead>
+          <tr>
+            <th>线程名</th>
+            <th>状态</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr class="item" v-for="thread in threads" :key="thread.id" v-on:click="getThreadDetail(thread.id)">
+            <td>{{ thread.name }}</td>
+            <td>{{ thread.state }}</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="col-md-6" style="overflow: scroll;">
+        <table class="table table-hover">
           <tbody>
           <tr>
-            <td>
-              divhaha
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              divhaha
-            </td>
+            <td><pre>{{ threadDetail }}</pre></td>
           </tr>
           </tbody>
         </table>
@@ -42,6 +55,8 @@
         loading: true,
         id: null,
         timer: null,
+        threads: [],
+        threadDetail: null,
         colors: ['#304ffe', '#b71c1c'],
         form: {columns: ['time', '活动线程', '峰值线程'], rows: []}
       }
@@ -61,10 +76,21 @@
           '活动线程': result.data.activeThreadCount,
           '峰值线程': result.data.peakThreadCount
         })
+      },
+      async getThreads() {
+        let result = await axios.get(`/api/jvms/${this.id}/threads`)
+
+        this.threads = result.data
+      },
+      async getThreadDetail(tid) {
+        let result = await axios.get(`/api/jvms/${this.id}/threads/${tid}`)
+        this.threadDetail = result.data
       }
     },
     created() {
       this.id = this.$route.params.id
+
+      this.getThreads()
 
       this.timer = setInterval(() => {
         this.getThreadsSummary();
@@ -77,4 +103,7 @@
 </script>
 
 <style scoped>
+  .item:hover {
+    cursor: pointer;
+  }
 </style>
