d17cb34227ed72e364cec8198d55a0d6ebc09c70
zhaozhentao <344696734@qq.com>
2020-02-08 09:54:53 +0800

    跟踪dist

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 npm run build
 
-sed -i 's/\/css/http:\/\/bozhen.live\/css/g' dist/index.html
-sed -i 's/\/js/http:\/\/bozhen.live\/js/g' dist/index.html
+sed -i '' 's/\/css/http:\/\/bozhen.live\/css/g' dist/index.html
+sed -i '' 's/\/js/http:\/\/bozhen.live\/js/g' dist/index.html
 
