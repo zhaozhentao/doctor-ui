168e677cf26bcc92e643557b57ee14d92d3e83a2
zhaozhentao <344696734@qq.com>
2020-02-08 09:45:22 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 npm run build
 
-sed -i '' 's/\/ui\/css/http:\/\/bozhen.live\/css/g' ../src/main/resources/public/ui/index.html
-sed -i '' 's/\/ui\/js/http:\/\/bozhen.live\/js/g' ../src/main/resources/public/ui/index.html
+sed -i '' 's/\/css/http:\/\/bozhen.live\/css/g' ./dist/index.html
+sed -i '' 's/\/js/http:\/\/bozhen.live\/js/g' ./dist/index.html
 
